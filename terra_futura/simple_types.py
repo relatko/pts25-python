40b78c14a75d8d91e3a751cf7aec543f5d38e739
vitaliikuzmenko3 <vkuzmenkov60@gmail.com>
2025-11-09 19:14:45 +0100

    Types added in file simple_types
    I added white classes such as Resource,Deck,CardSource,GameState
    Most like easy things like data classes and enumeration classes.
    Good things for start

--- terra_futura/simple_types.py
+++ terra_futura/simple_types.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+from typing import List
+from enum import Enum
+
 class GridPosition:
     _x: int
     _y: int
@@ -15,4 +19,54 @@ class GridPosition:
         return self._y
 
     def __str__(self) -> str:
-        return "("+str(self._x)+","+str(self._x)+")"
+        return "("+str(self._x)+","+str(self._y)+")"
+        
+class Resource(Enum):
+    GREEN = 1
+    RED = 2
+    YELLOW = 3
+    BULB = 4
+    GEAR = 5
+    CAR =6
+    MONEY = 7
+    POLUTION = 8
+    
+class Deck(Enum):
+    I = 1
+    II = 2
+
+class CardSource:
+    _deck: Deck
+    _index: int
+
+    def __init__(self, deck: Deck, index : int):
+        assert index >= 0, "Index CardSource init less 0"      
+        self._deck = deck
+        self._index = index
+
+    @property
+    def deck(self) -> Deck:
+        return self._deck
+
+    @deck.setter
+    def deck(self,value: Deck) -> None:
+        self._deck = value
+
+    @property
+    def index(self) -> int:
+        return self._index
+
+    @index.setter
+    def index(self,value: int) -> None:
+        assert value >= 0, "Index CardSource less 0 setter"      
+        self._index = value
+
+
+class GameState(Enum):
+    TAKE_CARD_NO_CARD_DISCARDED = 1
+    TAKE_CARD_CARD_DISCARDED = 2
+    ACTIVATE_CARD = 3
+    SELECT_REWARD = 4
+    SELECT_ACTIVATION_PATTERN = 5
+    SELECT_SCORING_METHOD = 6
+    FINISH = 7
