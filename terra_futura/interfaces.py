1ea0b1a5efd2e2845a45b1e6ecf0beda19415c51
Jatofi <jakubo.hlavaty2@gmail.com>
2025-11-27 22:51:00 +0100

    Declared some interfaces
    Declared interfaces for Grid, MoveCard, Pile and Card

--- terra_futura/interfaces.py
+++ terra_futura/interfaces.py
@@ -1,7 +1,27 @@
 # pylint: disable=unused-argument, duplicate-code
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+from terra_futura.simple_types import GridPosition, Resource
 
 
 class InterfaceActivateGrid:
     def set_activation_pattern(self, pattern: List[Tuple[int, int]]) -> None:
         assert False
+
+class InterfaceGrid:
+    def putCard(self, coordinate : GridPosition, card: InterfaceCard) -> bool:
+        assert False
+
+class InterfaceMoveCard:
+    """Interface for MoveCard"""
+    def moveCard(pile: InterfacePile, gridCoordinate: GridPosition, grid: InterfaceGrid) ->bool:
+        assert False
+        
+class InterfacePile:
+    def getCard(index:int) ->Optional[InterfaceCard]:
+        assert False
+
+class InterfaceCard:
+    def canGetResources(resources: List[Resource]) -> bool:
+        assert False
+
+    # treba implementovať zvyšok ...
