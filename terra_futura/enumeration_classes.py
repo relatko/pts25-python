e18a5109be90437154592e5677c0771196218dd5
Lujza104 <lujzakurimaiova@gmail.com>
2025-12-02 15:44:25 +0100

    Add files via upload

--- terra_futura/enumeration_classes.py
+++ terra_futura/enumeration_classes.py
@@ -17,3 +17,12 @@ class Resource(Enum):
 class CardSource(Enum):
     deck: Deck
     index: int
+
+class GameState(Enum):
+    TAKE_CARD_NO_CARD_DISCARDED = "TakeCardNoCardDiscarded"
+    TAKE_CARD_CARD_DISCARDED = "TakeCardCardDiscarded"
+    ACTIVATE_CARD = "ActivateCard"
+    SELECT_REWARD = "SelectReward"
+    SELECT_ACTIVATION_PATTERN = "SelectActivationPattern"
+    SELECT_SCORING_METHOD = "SelectScoringMethod"
+    FINISH = "Finish"
